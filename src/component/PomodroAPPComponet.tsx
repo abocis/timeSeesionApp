a1b46d640b111596227faff088f19042c6f418de
Abou cisse <90641949+abocis@users.noreply.github.com>
2024-11-30 02:00:02 +0100

    on going

--- src/component/PomodroAPPComponet.tsx
+++ src/component/PomodroAPPComponet.tsx
@@ -4,14 +4,23 @@ import TimedseessionComponet from "./TimedseesionComponent"
 
 const TOTAL_sESSION_IN_CYCLE = 4;
 
-const SESSION_TYPE_LABEL = {
-    
+const SESSION_TYPE_LABEL  = {
     focus: "Focus time",
-    "short-brach": "short break",
+    "short-break": "short break",
     "long-break": "long break",
 };
+const SESSION_DURATION_IN_MINUTES = {
+    focus:25,
+    "short-break":5,
+    "long-break": 20,
+};
+
 
 const PomodroAPPComponet = ()=> {
+    
+    const [currentSession, setCurrentSession] = useState(1);
+    const [currentSessionType, setCurrentSessionType] = useState("focus")
+
     return (
         <>
         <div style={{
@@ -21,9 +30,16 @@ const PomodroAPPComponet = ()=> {
             fontSize: "1.5 rem",
         }}
         >
-           <p>Session 1 of 4 </p>
+           <p>Session {currentSession} of {TOTAL_sESSION_IN_CYCLE}(
+            {SESSION_TYPE_LABEL[currentSessionType] })
+            </p>
+
+            <TimedseessionComponet
+            initialDuration={SESSION_DURATION_IN_MINUTES[currentSessionType]}
+            />
+
         </div>
-        <TimedseessionComponet/>
+        
       </>
     )
 }
